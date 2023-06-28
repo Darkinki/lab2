2804a9a9d701935f8e4e6a5583d2b311a702e599
Darkinki <bondarenkoas2004@gmail.com>
2023-06-28 13:17:01 +0300

    written new unit tests for linked lists

--- HeavyMemoryTests/UnitTest1.cs
+++ HeavyMemoryTests/UnitTest1.cs
@@ -1,16 +1,31 @@
 using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 [TestFixture]
-public class ProgramTests
+public class HeavyMemoryTests
 {
     [Test]
-    public void ProgramExecutionShouldntThrowException()
+    public void LinkedListAddLastIncreasesCount()
     {
-        TestDelegate testDelegate = () => {
-            Program.Main();
-        };
+        LinkedList<object> linkedList = new LinkedList<object>();
+        object obj = new object();
 
-        Assert.DoesNotThrow(testDelegate);
+        linkedList.AddLast(obj);
+
+        Assert.AreEqual(1, linkedList.Count);
+    }
+
+    [Test]
+    public void LinkedListAddLastAddsObjectToTheEnd()
+    {
+        LinkedList<object> linkedList = new LinkedList<object>();
+        object obj1 = new object();
+        object obj2 = new object();
+
+        linkedList.AddLast(obj1);
+        linkedList.AddLast(obj2);
+
+        Assert.AreEqual(obj2, linkedList.Last.Value);
     }
 }
